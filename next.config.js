7b016b64ffd0ac7846ec48c46267f155c49fc29d
Your Name <hkdas5464@gmail.com>
2025-02-15 19:21:07 +0530

    pusha

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 // next.config.js
 module.exports = {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          net: false,
-          tls: false,
-          fs: false,
-          child_process: false,
-          "util/types": false,
-        };
-      }
-      return config;
-    },
-  };
-  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        dns: false,
+        net: false,
+        tls: false,
+        fs: false,
+        child_process: false,
+        "util/types": false,
+      };
+    }
+    return config;
+  },
+};
